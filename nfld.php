ae9bff5fd171d9ad4fac2f88f0f94658c9abc0f9
Andrew Mason <andrewmasonmedia@gmail.com>
2018-12-09 14:16:39 -0500

    Update to nfld page, moved modal outside main container

--- nfld.php
+++ nfld.php
@@ -82,84 +82,15 @@ include 'includes/header.php';
 
     <div class="row d-flex text-secondary min-70 m-3 shadow-lg">
          <div class="col-lg-6 d-flex flex-column justify-content-center align-items-center text-left bg-img-nfld-banner min-50 p-0 m-0" data-aos="fade" data-aos-easing="ease-in-out-sine" data-aos-duration="1500"></div>
-         <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start bg-light text-left min-50 p-5 o-12">
-            <h2 class="section-title text-uppercase border border-bottom-0 p-4 m-0" data-aos="fade-down" data-aos-easing="ease-in-out-sine" data-aos-duration="1000">Booking + Info</h2>
-            <div class="wrap p-4 m-0 border" data-aos="fade-right" data-aos-easing="ease-in-out-sine" data-aos-duration="1500">
-                <p data-aos="fade" data-aos-easing="ease-in-out-sine" data-aos-duration="1500">To register, please contact Worldwide Quest at 416-633-5666, or toll free 1-800-387-1483 and ask for Donna, or email <a class="pink font-weight-bold" href="mailto:donna@worldwidequest.com">donna@worldwidequest.com.</a> TICO Registration number is 2667946.</p>
+         <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start bg-light text-left min-50 p-5 o-12" data-aos="fade-up" data-aos-easing="ease-in-out-sine" data-aos-duration="1500">
+            <h2 class="section-title text-uppercase border border-bottom-0 p-4 m-0">Booking + Info</h2>
+            <div class="wrap p-4 m-0 border">
+                <p>To register, please contact Worldwide Quest at 416-633-5666, or toll free 1-800-387-1483 and ask for Donna, or email <a class="pink font-weight-bold" href="mailto:donna@worldwidequest.com">donna@worldwidequest.com.</a> TICO Registration number is 2667946.</p>
              </div>
              <div class="button-wrap border border-top-0 p-3">
                 <!-- Button trigger modal -->
                 <button class="btn btn-lg btn-dark" data-toggle="modal" data-target="#nfldModal">More Info</button>
-                <!-- Modal -->
-                <div class="modal fade" id="nfldModal" tabindex="-1" role="dialog" aria-labelledby="Newfoundland" aria-hidden="true">
-                  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="nfldModalTitle">WiWP: Newfoundland</h5>
-                        <button type="button" class="close h2" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true"><i class="fa fa-plus"></i></span>
-                        </button>
-                      </div>
-                      <div class="modal-body bg-cubes p-4">
-                         <div class="jumbotron jumbotron-fluid shadow-lg bg-img-nfld-banner p-4 mb-3 animated fadeIn">
-                            <div class="wrap h-100 w-100 p-4 border border-light">
-                             <h2 class="text-light font-weight-light font-italic mb-0 pb-0 animated fadeInRight">The Doctor's House</h2>
-                             <p class="text-light font-weight-light font-italic pt-0 animated fadeInLeft">June 2-7, 2019 · Green’s Harbour Newfoundland</p>
-                             </div>
-                         </div>
-                            <h5 class="font-weight-bold border-top py-3 m-0">Dates: June 2-7, 2019</h5>
-                            <h5 class="font-weight-bold py-3 border-top m-0">Cost: $3950 CAD + HST</h5>
-                            <h5 class="font-weight-bold border-top border-bottom py-3">Price includes</h5>
-                           <ul class="py-3">
-                                <li>7 nights accommodation: Each person has their own room with ensuite.</li>
-                                <li>Ground transportation from St. John's to The Doctor's House and return (at specified arrival and departure dates/times only).</li>
-                                <li>Three gourmet meals each day.</li>
-                                <li>Includes all gratuities.</li>
-                                <li>6-day workshop with one day off mid-week.</li>
-                                <li>Limited to 8 people for lots of individual attention.</li>
-                                <li>Boat trip on day off: 3 hours  to hopefully see icebergs and perhaps whales.</li>
-                                <li>Lots of time for walking, sketching, journaling, connecting with the land.</li>
-                                <li>Workshop rental space is in the Upper Loft of the barn which has a panoramic view. We can leave supplies set up.</li>
-                                <li>Tea/coffee during the workshop.</li>
-                                <li>Certain workshop materials: Cold Wax medium, vinyl gloves, tissue paper, shop towels (1 roll each), vegetable oil for clean-up, 1 scraper/squeegee tool per person.</li>
-                           </ul>
-                           <h5 class="font-weight-bold border-top border-bottom py-3">Does not include</h5>
-                           <ul class="py-3">
-                               <li>Airfare</li>
-                               <li>Medical/cancellation insurance. International participants will need medical insurance. It is suggested that everyone purchase cancellation insurance.
-                               <li>Alcoholic beverages.</li>
-                               <li>Spa services.</li>
-                               <li>Painting materials except those included. Materials List provided.</li>
-                          </ul>
-                          <h5 class="font-weight-bold border-top border-bottom py-3">Payment Schedule</h5>
-                          <ul class="py-3">
-                               <li>Deposit of $500.00 CAD to hold your place. Due on registration.</li>
-                               <li>Deadline: August 15th. This deposit is non-refundable but it is transferable to another of Janice Mason Steeves' trips.</li>
-                               <li>Interim fee: $1000 due December 1st, 2018.</li>
-                               <li>Final fee: $2963.50.00 CAD to be paid by March 1, 2019.</li>
-                               <li>We cannot accept payment by e-transfer.</li>
-                               <li>We provide a 1% discount for cheque payment.</li>
-                               <li>All payments are non-refundable.</li>
-                          </ul>
-                          <!--book-->
-                          <h5 class="font-weight-bold border-top border-bottom py-3">Cancellation policy</h5>
-                          <p class="py-3">We reserve the right to cancel any trip prior to departure. Full refund constitutes final settlement. For questions related to the workshop and materials, please contact us.</p>
-                          <!--book-->
-                          <h5 class="font-weight-bold border-top border-bottom py-3">Booking + Contact</h5>
-                          <p class="pt-3">To book your spot, please contact Donna at Worldwide Quest:</p>
-                          <ul>
-                              <li>416-633-5666</li>
-                              <li>toll free 1-800-387-1483</li>
-                              <li>or email: <a class="font-weight-bold pink" href="mailto:donna@worldwidequest.com">donna@worldwidequest.com</a></li>
-                              <li>TICO Registration number is 2667946.</li>
-                          </ul>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                      </div>
-                    </div>
-                  </div>
-                </div><!--end modal-->
+                <!--modal content-->
              </div><!--end wrap-->
          </div><!--end col-->
      </div><!--end row-->
@@ -177,6 +108,78 @@ include 'includes/header.php';
       </div>
       <!--end row--> 
     </div><!--end container-->
+    
+    <!-- Modal outside main container for animation -->
+    <div class="modal fade" id="nfldModal" tabindex="-1" role="dialog" aria-labelledby="Newfoundland" aria-hidden="true">
+      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="nfldModalTitle">WiWP: Newfoundland</h5>
+            <button type="button" class="close h2" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true"><i class="fa fa-plus"></i></span>
+            </button>
+          </div>
+          <div class="modal-body bg-cubes p-4">
+             <div class="jumbotron jumbotron-fluid shadow-lg bg-img-nfld-banner p-4 mb-3 animated fadeIn">
+                <div class="wrap h-100 w-100 p-4 border border-light">
+                 <h2 class="text-light font-weight-light font-italic mb-0 pb-0 animated fadeInRight">The Doctor's House</h2>
+                 <p class="text-light font-weight-light font-italic pt-0 animated fadeInLeft">June 2-7, 2019 · Green’s Harbour Newfoundland</p>
+                 </div>
+             </div>
+                <h5 class="font-weight-bold border-top py-3 m-0">Dates: June 2-7, 2019</h5>
+                <h5 class="font-weight-bold py-3 border-top m-0">Cost: $3950 CAD + HST</h5>
+                <h5 class="font-weight-bold border-top border-bottom py-3">Price includes</h5>
+               <ul class="py-3">
+                    <li>7 nights accommodation: Each person has their own room with ensuite.</li>
+                    <li>Ground transportation from St. John's to The Doctor's House and return (at specified arrival and departure dates/times only).</li>
+                    <li>Three gourmet meals each day.</li>
+                    <li>Includes all gratuities.</li>
+                    <li>6-day workshop with one day off mid-week.</li>
+                    <li>Limited to 8 people for lots of individual attention.</li>
+                    <li>Boat trip on day off: 3 hours  to hopefully see icebergs and perhaps whales.</li>
+                    <li>Lots of time for walking, sketching, journaling, connecting with the land.</li>
+                    <li>Workshop rental space is in the Upper Loft of the barn which has a panoramic view. We can leave supplies set up.</li>
+                    <li>Tea/coffee during the workshop.</li>
+                    <li>Certain workshop materials: Cold Wax medium, vinyl gloves, tissue paper, shop towels (1 roll each), vegetable oil for clean-up, 1 scraper/squeegee tool per person.</li>
+               </ul>
+               <h5 class="font-weight-bold border-top border-bottom py-3">Does not include</h5>
+               <ul class="py-3">
+                   <li>Airfare</li>
+                   <li>Medical/cancellation insurance. International participants will need medical insurance. It is suggested that everyone purchase cancellation insurance.
+                   <li>Alcoholic beverages.</li>
+                   <li>Spa services.</li>
+                   <li>Painting materials except those included. Materials List provided.</li>
+              </ul>
+              <h5 class="font-weight-bold border-top border-bottom py-3">Payment Schedule</h5>
+              <ul class="py-3">
+                   <li>Deposit of $500.00 CAD to hold your place. Due on registration.</li>
+                   <li>Deadline: August 15th. This deposit is non-refundable but it is transferable to another of Janice Mason Steeves' trips.</li>
+                   <li>Interim fee: $1000 due December 1st, 2018.</li>
+                   <li>Final fee: $2963.50.00 CAD to be paid by March 1, 2019.</li>
+                   <li>We cannot accept payment by e-transfer.</li>
+                   <li>We provide a 1% discount for cheque payment.</li>
+                   <li>All payments are non-refundable.</li>
+              </ul>
+              <!--book-->
+              <h5 class="font-weight-bold border-top border-bottom py-3">Cancellation policy</h5>
+              <p class="py-3">We reserve the right to cancel any trip prior to departure. Full refund constitutes final settlement. For questions related to the workshop and materials, please contact us.</p>
+              <!--book-->
+              <h5 class="font-weight-bold border-top border-bottom py-3">Booking + Contact</h5>
+              <p class="pt-3">To book your spot, please contact Donna at Worldwide Quest:</p>
+              <ul>
+                  <li>416-633-5666</li>
+                  <li>toll free 1-800-387-1483</li>
+                  <li>or email: <a class="font-weight-bold pink" href="mailto:donna@worldwidequest.com">donna@worldwidequest.com</a></li>
+                  <li>TICO Registration number is 2667946.</li>
+              </ul>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div><!--end modal-->
+                
 </main>
 
 <?php
